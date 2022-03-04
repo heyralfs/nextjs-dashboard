5137edb254dd6c695f3993b4675a4deb6897b97a
Ralf Oliveira Ferreira <ralfoliveira@gmail.com>
2022-03-03 21:15:15 -0300

    Sidebar responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+	const { onOpen } = useSidebarDrawer();
+
 	const isWideVersion = useBreakpointValue({
 		base: false,
 		lg: true,
@@ -21,6 +25,19 @@ export function Header() {
 			align="center"
 			px="6"
 		>
+			{!isWideVersion && (
+				<IconButton
+					icon={<Icon as={RiMenuLine} />}
+					fontSize="24"
+					variant="unstyled"
+					onClick={onOpen}
+					aria-label="Open navigation"
+					mr="2"
+					display="flex"
+					alignItems="center"
+				></IconButton>
+			)}
+
 			<Logo />
 
 			{isWideVersion && <SearchBox />}
